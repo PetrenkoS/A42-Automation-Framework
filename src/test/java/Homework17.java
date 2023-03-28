8e27a6aebb10048aad2f352fa6549d9f81e6d53f
annastonaieva <anna.stonaieva@testpro.io>
2023-03-28 12:16:30 -0700

    initial changes in 17

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,35 +1,45 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class Homework17 extends BaseTest {
 
+    private WebElement viewAllBtn;
+
     @Test
-    public void addSongToPlaylist() throws InterruptedException {
+    public void addSongToPlayList() {
 
-        String newSongAddedNotificationText = "Added 1 song into";
+        String song = "Dark Days";
+        login ("anna.stonaieva@testpro.io", "te$t$tudent");
 
-        navigateToPage();
-        provideEmail("anna.stonaieva @testpro.io");
-        providePassword("te$t$tudent");
-        clickSubmit();
-        searchSong(songTitleKeyword:"Dark Days");
-        clickViewAllSearchResult();
-        selectFirstSongResult();
-        clickAddToBtn();
-        choosePlayList();
-        Assert.assertTrue(getNotificationText().contains(newSongAddedNotificationText));
-    }
+        WebElement searchField = driver.findElement(By.cssSelector("[type='search']"));
+        searchField.click();
+        searchField.clear();
+        searchField.sendKeys(song);
 
-    private void providePassword(String password) {
+        WebElement viewAllBtn = driver.findElement(By.cssSelector("[data-test='view-all-songs-btn']"));
+        viewAllBtn.click();
 
-    }
+        List<WebElement> songResults = driver.findElements(By.cssSelector("#songResultsWrapper .song-item"));
+        songResults.get(0).click();
 
-    private void provideEmail(String email) {
+        WebElement addToBtn = driver.findElement(By.cssSelector("[data-test='add-to-btn']"));
+        addToBtn.click();
 
-    }
-
-    private void navigateToPage() {
-
+        WebElement listNameField = driver.findElement(By.cssSelector("data-test='new-playlist-name'"));
+        listNameField.click();
+        listNameField.clear();
+        listNameField.sendKeys ("My Playlist");
+        new Actions(driver)
+                .keyDown(Keys.ENTER)
+                .perform();
+        WebElement successBanner = driver.findElement(By.cssSelector(".success.show"));
+        Assert.assertTrue(successBanner.isDisplayed());
     }
 }
