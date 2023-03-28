9747e2fb818f23c574015403cbcb7faf1a65b34a
annastonaieva <anna.stonaieva@testpro.io>
2023-03-28 10:39:54 -0700

    initial changes in 17

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,8 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
 
 public class BaseTest {
 
+    @BeforeSuite
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
 }
--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,4 +1,6 @@
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class Homework17 extends BaseTest {
 
@@ -8,7 +10,7 @@ public class Homework17 extends BaseTest {
         String newSongAddedNotificationText = "Added 1 song into";
 
         navigateToPage();
-        provideEmail(anna.stonaieva @testpro.io);
+        provideEmail("anna.stonaieva @testpro.io");
         providePassword("te$t$tudent");
         clickSubmit();
         searchSong(songTitleKeyword:"Dark Days");
@@ -17,5 +19,17 @@ public class Homework17 extends BaseTest {
         clickAddToBtn();
         choosePlayList();
         Assert.assertTrue(getNotificationText().contains(newSongAddedNotificationText));
+    }
+
+    private void providePassword(String password) {
+
+    }
+
+    private void provideEmail(String email) {
+
+    }
+
+    private void navigateToPage() {
+
     }
 }
