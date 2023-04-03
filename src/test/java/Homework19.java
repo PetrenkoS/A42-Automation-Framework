ca250f87bea22a66a4cfc7e84188abb900bc6163
annastonaieva <anna.stonaieva@testpro.io>
2023-04-03 12:43:29 -0700

    HW19 (20)

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 public class Homework19 extends BaseTest  {
@@ -7,19 +9,20 @@ public class Homework19 extends BaseTest  {
     @Test
     public void Login() {
 
+        //GIVEN:
         login("anna.stonaieva@testpro.io", "te$tStudent");
     }
-        public void openPlaylist() throws InterruptedException {
-            WebElement Playlist = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
-            Playlist.click();
-            Thread.sleep(1000);
-        }
 
+    //WHEN:
+        public void openPlaylist() throws InterruptedException {
+            WebElement Playlist = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".playlist:nth-child(3)"));
+            Playlist.click();
+
+        }
+//THEN:
         public void deletePlaylist() throws InterruptedException {
-        WebElement deletePlaylistButton = driver.findElement(By.cssSelector(".btn-delete-playlist"));
+        WebElement deletePlaylistButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".btn-delete-playlist"));
         deletePlaylistButton.click();
-        Thread.sleep(1000);
+        }
     }
 
-
-}
